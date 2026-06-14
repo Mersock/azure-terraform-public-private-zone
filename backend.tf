921cabfcf4a33e7ebc3b84f473aa2090c879c922
Phumthawan Nokun <knzphumthawan@gmail.com>
2026-06-14 19:54:38 +0700

    add backend provider

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "1.15.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.77.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.9.0"
+    }
+  }
+}
